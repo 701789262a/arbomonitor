80d6b64d045735c9e2e0e5c1ac3e3a54295755a2
Paulo <innorammara@gmail.com>
2021-03-21 16:41:51 +0100

    commit

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def reporter(q):
             if d.sort_values("latency")["status"].iloc[0] and my_ts - int(
                     d.sort_values("latency")["timestamp"].iloc[0]) > 45:
                 print("INVIA RICHIESTA PER DIVENTARE TRADER A %s PER DOWNTIME SERVER MIGLIORE" % (
-                    d.sort_values("latency")["address"].iloc[0]))
+                    d.sort_values(["status", "latency"], ascending=[False, True])["address"].iloc[0]))
                 # PRENDI IL SERVER UP CON LATENZA MIGLIORE E INVIAGLI UN GO
                 # AL SERVER IN QUESTIONE NELL IF BISOGNA INVIARE PREVENTIVAMENTE UN SEGNALE DI STOP
                 ans = (say(d.sort_values(["status", "latency"], ascending=[False, True])["address"].iloc[0], "GO"),
