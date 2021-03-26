1b4ba0703034994acc167a979bc86c011bcb7fef
Paulo <innorammara@gmail.com>
2021-03-26 17:03:39 +0100

    commit

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def reporter(q):
                        tablefmt='psql'))
         my_ts = int(datetime.datetime.now(datetime.timezone.utc).timestamp())
         try:
-            print(q_key.empty())
+            print("e vuota?",q_key.empty())
             if not q_key.empty:
                 print("ciao")
                 print(q_key.get())
